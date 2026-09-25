e631c17f84cef9eedfcaf45574a2dea637096796
Kirill Zaytsev <kosher12jew@yandex.ru>
2026-09-25 14:30:05 +0300

    Added descriptions for each function

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    #Получаем на вход радиус круга. Возвращаем площадь всей фигуры
     return math.pi * r * r
 
 
 def perimeter(r):
+    #Получаем на вход радиус круга. Возвращаем длину окружности
     return 2 * math.pi * r
 
